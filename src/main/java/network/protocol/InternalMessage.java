dc4c06e0934b81cec1a2b049eb80f331c08ead02
hbccdf <hbccdf@126.com>
2018-08-31 15:12:46 +0800

    add internal message

--- src/main/java/network/protocol/InternalMessage.java
+++ src/main/java/network/protocol/InternalMessage.java
@@ -1,4 +1,46 @@
 package network.protocol;
 
-public class InternalMessage {
+public class InternalMessage extends BaseMessage<InternalMessageHeader> {
+
+    public InternalMessage(int cmdId, byte[] data) {
+        super(new InternalMessageHeader(cmdId), data);
+    }
+
+    public int getConnId() {
+        return this.header.getConnId();
+    }
+
+    public void setConnId(int connId) {
+        this.header.setConnId(connId);
+    }
+
+    public short getFromId() {
+        return this.header.getFromId();
+    }
+
+    public void setFormId(short formId) {
+        this.header.setFromId(formId);
+    }
+
+    public short getToId() {
+        return this.header.getToId();
+    }
+
+    public void setToId(short toId) {
+        this.header.setToId(toId);
+    }
+
+    public int getSeqId() {
+        return header.getSeqId();
+    }
+
+    public void setSeqId(int seqId) {
+        this.header.setSeqId(seqId);
+    }
+
+    @Override
+    public String toString() {
+        return "InternalMessage [getFromId()=" + getFromId() + ", getToId()=" + getToId() + ", getConnId()="
+                + getConnId() + ", getSeqId()=" + getSeqId() + ", getCommandId()=" + getCmdId() + "]";
+    }
 }
--- src/main/java/network/protocol/InternalMessageHeader.java
+++ src/main/java/network/protocol/InternalMessageHeader.java
@@ -1,4 +1,45 @@
 package network.protocol;
 
-public class InternalMessageHeader {
+public class InternalMessageHeader extends BaseMessageHeader{
+    protected short fromId;
+    protected short toId;
+    protected int connId;
+    protected int seqId;
+
+    public InternalMessageHeader(int cmdId) {
+        super();
+        this.cmdId = cmdId;
+    }
+
+    public int getConnId() {
+        return connId;
+    }
+
+    public void setConnId(int connId) {
+        this.connId = connId;
+    }
+
+    public short getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(short fromId) {
+        this.fromId = fromId;
+    }
+
+    public short getToId() {
+        return toId;
+    }
+
+    public void setToId(short toId) {
+        this.toId = toId;
+    }
+
+    public int getSeqId() {
+        return seqId;
+    }
+
+    public void setSeqId(int seqId) {
+        this.seqId = seqId;
+    }
 }
