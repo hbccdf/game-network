f81c878ad92ce55465d2ba7bcc5d5fe6b2bae983
hbccdf <hbccdf@126.com>
2018-10-24 16:08:27 +0800

    add protocolManager

---
+++ src/main/java/network/protocol/ProtocolManager.java
@@ -0,0 +1,21 @@
+package network.protocol;
+
+import java.util.HashMap;
+
+public class ProtocolManager {
+    private static HashMap<Class<?>, Integer> classAndIdMap = new HashMap<>();
+    private static HashMap<Integer, Class<?>> idAndClassMap = new HashMap<>();
+
+    public static void register(Class<?> clz, Integer protocolId) {
+        classAndIdMap.put(clz, protocolId);
+        idAndClassMap.put(protocolId, clz);
+    }
+
+    public static Integer getId(Class<?> clz) {
+        return classAndIdMap.get(clz);
+    }
+
+    public static Class<?> getClass(Integer protocolId) {
+        return idAndClassMap.get(protocolId);
+    }
+}
