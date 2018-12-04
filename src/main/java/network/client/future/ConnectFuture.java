c08d83e79660fc4b8c89c6d5605daa9095ae3172
hbccdf <hbccdf@126.com>
2018-12-04 11:53:18 +0800

    add future

--- src/main/java/network/client/future/ConnectFuture.java
+++ src/main/java/network/client/future/ConnectFuture.java
@@ -1,31 +1,5 @@
 package network.client.future;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+public class ConnectFuture extends DefaultFuture {
 
-public class ConnectFuture extends BaseFuture<Boolean> {
-    private static final int DEFAULT_TIME_OUT = 10000;
-
-    private boolean result = false;
-    @Override
-    public Boolean get() throws InterruptedException {
-        try {
-            return get(DEFAULT_TIME_OUT, TimeUnit.MILLISECONDS);
-        } catch (TimeoutException e) {
-
-        }
-        return false;
-    }
-
-    @Override
-    public Boolean get(long timeout, TimeUnit unit) throws InterruptedException, TimeoutException {
-        wait(timeout, unit);
-        return result;
-    }
-
-    public void setResult(boolean result) {
-        this.result = result;
-
-        complete();
-    }
 }
